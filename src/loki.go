79c579022e6712ea1264174217d31587d7039ab8
Andre <agu3rra@gmail.com>
2020-05-02 17:00:50 -0300

    it begins

--- src/loki.go
+++ src/loki.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Welcome to GOSS")
+	fmt.Println("Welcome to Loki the Open Source Security Scanner!")
+
+	// Arguments
+	dependenciesPtr := flag.String("dependencies", "", "Path to your dependencies file.")
+	languagePtr := flag.String("language", "", "JavaScript, Python, Ruby, Java, PHP, DOTNET")
+	flag.Parse()
+
+	// Startup messages
+	fmt.Println("Dependencies file:", *dependenciesPtr)
+	fmt.Println("Language:", *languagePtr)
+	fmt.Println("To supress and justify (optionally) findings, simply provide a gos3ignore.yml file.")
+
 }
